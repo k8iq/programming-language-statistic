b818255f388608b643e4c3631cf638aca263410b
Yasong Li <liyasong1987x@gmail.com>
2020-05-30 13:30:37 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,93 @@
-# programming-language-statistic
-将 Github 上的项目按创建年份及编程语言进行统计
+# Github Programing Language Statistics
+
+Statistics on the number of new project by programming language according to Github search
+
+Automatic update via Github Action
+
+## Programming Language 使用趋势
+
+![line](images/line.png)
+
+## 新增项目(Programming Language)分布图
+
+![bar](images/bar.png)
+
+## 年度新增项目(Programming Language)分布图
+
+![2020](images/pie_2020.png)
+
+![2019](images/pie_2019.png)
+
+![2018](images/pie_2018.png)
+
+![2017](images/pie_2017.png)
+
+![2016](images/pie_2016.png)
+
+![2015](images/pie_2015.png)
+
+![2014](images/pie_2014.png)
+
+![2013](images/pie_2013.png)
+
+![2012](images/pie_2012.png)
+
+![2011](images/pie_2011.png)
+
+![2010](images/pie_2010.png)
+
+![2009](images/pie_2009.png)
+
+![2008](images/pie_2008.png)
+
+## Usage
+
+```python
+python pls.py
+```
+
+Statistics on the number of new project by programming language according to Github search
+
+Automatic update via Github Action
+
+## 主流编程语言新增项目走势图
+
+![line](images/line.png)
+
+## 新增项目编程语言柱状图
+
+![bar](images/bar.png)
+
+## 新增项目编程语言分布图
+
+![2020](images/pie_2020.png)
+
+![2019](images/pie_2019.png)
+
+![2018](images/pie_2018.png)
+
+![2017](images/pie_2017.png)
+
+![2016](images/pie_2016.png)
+
+![2015](images/pie_2015.png)
+
+![2014](images/pie_2014.png)
+
+![2013](images/pie_2013.png)
+
+![2012](images/pie_2012.png)
+
+![2011](images/pie_2011.png)
+
+![2010](images/pie_2010.png)
+
+![2009](images/pie_2009.png)
+
+![2008](images/pie_2008.png)
+
+## Usage
+
+```python
+python pls.py
+```
---
+++ pls.py
@@ -0,0 +1,163 @@
+#!usr/bin/env python3
+
+import time
+import requests
+from datetime import datetime
+import pandas as pd
+import matplotlib.pyplot as plt
+from bs4 import BeautifulSoup
+
+
+def request_by_year(year, stars):
+    return request(f"{year}-01-01", f"{year}-12-31", stars)
+
+
+def request(begin, end, stars):
+    print(f"[DEBUG] Begin<{begin}> End<{end}> Stars<{stars}>")
+    url = f"https://github.com/search?q=created%3A{begin}..{end}+stars%3A%3E{stars}&type=Repositories"
+    r = requests.get(url)
+    if r.status_code != 200:
+        print(f"request error [r.status_code]: {r.text}")
+        return
+    return BeautifulSoup(r.text, "html.parser")
+
+
+def parser_total(soup):
+    for h3 in soup.find_all("h3"):
+        tmp = h3.get_text()
+        if "repository results" in tmp:
+            lst = [item for item in tmp.split() if item]
+            if len(lst) > 0:
+                return int(lst[0].replace(",", ""))
+
+
+def parser_program(soup, other=True):
+    result = list()
+    for ul in soup.find_all("ul"):
+        if "filter-list" in ul.get_attribute_list("class"):
+            for li in ul.find_all("li"):
+                lst = [item for item in li.get_text().split() if item]
+                if len(lst) == 2:
+                    result.append(lst)
+
+    sizes = [int(item[0].replace(",", "")) for item in result]
+    labels = [item[1] for item in result]
+
+    if other:
+        total = parser_total(soup)
+        if total:
+            sizes.append(total-sum(sizes))
+            labels.append("Others")
+
+    return sizes, labels
+
+
+def pie(year, stars, sizes, labels):
+    plt.pie(sizes, labels=labels, autopct='%1.1f%%',
+            shadow=True, startangle=600)
+    plt.axis('equal')
+
+    plt.title(
+        f"""    
+Number of New Projects
+[Year: {year}; Stars > {stars}; Source: Github Search]
+time: {datetime.now().isoformat()}
+""")
+    plt.tight_layout()
+    plt.savefig(f"./images/pie_{year}.png")
+    plt.close()
+
+
+def line(lst, stars, includes=["Python", "Java", "Go", "TypeScript", "JavaScript"]):
+    years = [item[0] for item in lst]
+    totals = [sum(item[1]) for item in lst]
+    l = len(years)
+    lst = [dict(zip(item[2], item[1])) for item in lst]
+    r = dict()
+
+    for idx, d in enumerate(lst):
+        for k, v in d.items():
+            if k not in r:
+                r[k] = [0] * l
+            r[k][idx] = v/totals[idx] * 100
+
+    for k, v in r.items():
+        if k in includes:
+            plt.plot([str(year) for year in years], v, label=k)
+
+    plt.title(
+        f"""    
+Percent of New Projects
+[Stars > {stars}; Source: Github Search]
+time: {datetime.now().isoformat()}
+""")
+    plt.xlabel("Years")
+    plt.ylabel("Percent of New Projects")
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig("./images/line.png")
+    plt.close()
+
+
+def bar(lst, stars):
+    years = [item[0] for item in lst]
+    dat = [dict(zip(item[2], item[1])) for item in lst]
+    totals = [sum(item[1]) for item in lst]
+    l = len(years)
+
+    d = dict()
+    for idx, year in enumerate(years):
+        for k, v in dat[idx].items():
+            if k not in d:
+                d[k] = [0] * l
+            d[k][idx] = v
+            d[k][idx] = v/totals[idx] * 100
+
+    df = pd.DataFrame.from_dict(d, orient="index", columns=years)
+    df.plot.bar(
+        title=f"""
+Percent of New Projects through Programming Languages 
+[Stars > {stars}; Source: Github Search; {datetime.now().isoformat()}]
+""")
+
+    plt.xlabel("Programming Languages")
+    plt.ylabel("Percent of New Projects")
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig("./images/bar.png")
+    plt.close()
+
+
+def statistics_all(stars=10, interval=5):
+    lst = list()
+    years = list(range(2008, 2021))
+    i = 0
+    while i < len(years):
+        soup = request_by_year(years[i], stars)
+        if not soup:
+            continue
+
+        sizes, labels = parser_program(soup)
+        lst.append((years[i], sizes, labels))
+        i += 1
+        if i < len(years):
+            time.sleep(interval)
+    return lst
+
+
+def run():
+    lst = statistics_all(10)
+
+    # pie all
+    for item in lst:
+        pie(item[0], 10, item[1], item[2])
+
+    # bar all
+    bar(lst[-5:], 10)
+
+    # line
+    line(lst, 10)
+
+
+if __name__ == "__main__":
+    run()
